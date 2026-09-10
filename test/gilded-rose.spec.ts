6002195b56de440d59556c446a80258ec8e126aa
stanleyctg03 <stanley.ching@softwire.com>
2026-09-10 12:27:08 +0100

    Written test for quality decrease and sulfares

--- test/gilded-rose.spec.ts
+++ test/gilded-rose.spec.ts
@@ -9,4 +9,18 @@ describe('Gilded Rose', function () {
         expect(items[0].name).to.equal('foo');
     });
 
+    it('should decrease twice as fast when sell by date has passed for normal items', function () {
+        const items = [new Item('Apple', 0, 20)];
+        const gildedRose = new GildedRose(items);
+        const updatedItems = gildedRose.updateQuality();
+        expect(updatedItems[0].quality).to.equal(18);
+    });
+
+    it('should never be sold or decrease in quality for Sulfaras', function () {
+        const items = [new Item('Sulfuras, Hand of Ragnaros', 12, 20)];
+        const gildedRose = new GildedRose(items);
+        const updatedItems = gildedRose.updateQuality();
+        expect(updatedItems[0].quality).to.equal(20);
+        expect(updatedItems[0].sellIn).to.equal(12);        
+    });
 });
